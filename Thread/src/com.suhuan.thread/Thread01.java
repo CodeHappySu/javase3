27928a7d1b4f11a41fd3a3b17a8d9120fe2b4369
suhuan <1052417877@qq.com>
2022-10-08 20:20:18 +0800

    commit1

---
+++ Thread/src/com.suhuan.thread/Thread01.java
@@ -0,0 +1,14 @@
+package com.suhuan.thread;
+
+/**
+ * @Auther: suhuan
+ * @Date: 2022/10/8 - 10 - 08 - 20:14
+ */
+public class Thread01 {
+
+    public static void main(String[] args) {
+        System.out.println("多线程1");
+        System.out.println("多线程2");
+    }
+
+}
